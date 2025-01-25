f430512c72b15377d6d45fea5b06b67831e725c0
João Teles <joaohts2210@gmail.com>
2025-01-25 09:58:20 -0300

    exercicio 9 pronto e funcionando

--- javascript/exercicios/exc9/index.js
+++ javascript/exercicios/exc9/index.js
@@ -1,4 +1,3 @@
-/*
 var pais = 'EUA'
 console.log(`Vivendo em ${pais}`)
 if (pais == 'Brasil') {
@@ -7,6 +6,3 @@ if (pais == 'Brasil') {
 else {
     console.log(`You're foreign`)
 }
-*/
-
-//Desafio para depois, fazer ele mudar a lingua em diz aonde vive, dependendo do pais em que a pessoa mora, fazer me forma de site
