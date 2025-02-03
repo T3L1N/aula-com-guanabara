581f7105b030d03a677c3529f6c69100577a130c
João Teles <joaohts2210@gmail.com>
2025-02-03 17:44:18 +0000

    exercicio 15 funcionante

--- javascript/exercicios/exc15/script.js
+++ javascript/exercicios/exc15/script.js
@@ -1,7 +1,55 @@
-var msg = document.getElementById('msg')
-var img = document.getElementById('img')
-
-
-//e2cd9f
-//fe8c3f
-//2c5392
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gen = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gen = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/menino.png')
+            } else if (idade < 21) {
+                //rapaz
+                img.setAttribute('src', 'imagens/rapaz.png')
+            } else if (idade < 55) {
+                //adulto
+                img.setAttribute('src', 'imagens/adulto.png')
+            } else if (idade < 100) {
+                //veio
+                img.setAttribute('src', 'imagens/veio.png')
+            } else {
+                //difunto
+                img.setAttribute('src', 'imagens/difunto.png')
+            }
+        } else if (fsex[1].checked) {
+            gen = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/menina.png')
+            } else if (idade < 21) {
+                //rapaz
+                img.setAttribute('src', 'imagens/moca.png')
+            } else if (idade < 55) {
+                //adulto
+                img.setAttribute('src', 'imagens/adulta.png')
+            } else if (idade < 100) {
+                //veio
+                img.setAttribute('src', 'imagens/veia.png')
+            } else {
+                //difunto
+                img.setAttribute('src', 'imagens/difunto.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Você é ${gen} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
