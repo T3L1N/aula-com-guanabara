4e9f15c3a882a85f9c8356f766c975f7a0c848db
João Teles <joaohts2210@gmail.com>
2025-02-04 18:50:34 +0000

    exc18 pronto

--- javascript/exercicios/exc18/script.js
+++ javascript/exercicios/exc18/script.js
@@ -2,5 +2,32 @@ function contar() {
     var inc = document.querySelector('input#inc')
     var fim = document.querySelector('input#fim')
     var pas = document.querySelector('input#pas')
-    
+    var res = document.querySelector('div#res')
+
+    if (inc.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
+        res.innerHTML = 'Não é possivel contar'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(inc.value)
+        var f = Number(fim.value)
+        var p = Number(pas.value)
+
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+            //Contagem crescente
+            for(var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //Contagem regressiva
+            for(var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
