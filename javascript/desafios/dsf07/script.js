6d9305cdc7adb20ac5d8d8499b60b53e663b7e02
João Teles <joaohts2210@gmail.com>
2025-02-06 19:09:04 +0000

    exc 7 pronto

---
+++ javascript/desafios/dsf07/script.js
@@ -0,0 +1,16 @@
+function calcular() {
+    var cot = document.querySelector('input#cot')
+    var real = document.querySelector('input#real')
+    var res = document.querySelector('div#res')
+
+    if (cot.value == 0 || real.value === 0 || cot.value.length == 0 || real.value.length == 0) {
+        alert('[ERRO] Digite valor corretamente')
+    } else {
+        var c = Number(cot.value)
+        var r = Number(real.value)
+        var result = r / c
+
+        res.innerHTML = `Com R$${r.toFixed(2).replace('.', ',')} você tem US$${result.toFixed(2).replace('.', ',')}`
+    }
+
+}
