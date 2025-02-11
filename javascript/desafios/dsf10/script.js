0eb5d6c3688e747736d3df5bbc1eb79d1a54956e
João Teles <joaohts2210@gmail.com>
2025-02-11 19:17:06 +0000

    melhorias no desafio 10

--- javascript/desafios/dsf10/script.js
+++ javascript/desafios/dsf10/script.js
@@ -1,7 +1,12 @@
+function formatarNumero(n) {
+    return n < 0 ? `(${n})` : n
+}
+
 function calcular() {
     var numa = document.querySelector('input#numa')
     var numb = document.querySelector('input#numb')
     var numc = document.querySelector('input#numc')
+    var igual = document.querySelector('input#igual')
     var res = document.querySelector('div#res')
 
     if (numa.value.length == 0 || numa.value == 0 || numb.value.length == 0 || numb.value == 0 || numc.value.length == 0 || numc.value == 0) {
@@ -10,59 +15,63 @@ function calcular() {
         var na = Number(numa.value)
         var nb = Number(numb.value)
         var nc = Number(numc.value)
+        var ig = Number(igual.value)
         var delta = nb **2 - 4 * na * nc
 
-
-        res.innerHTML = `<p>A equação atual é ${na}x<sup>2</sup> + ${nb}x + ${nc} = 0</p>`
-        res.innerHTML += `<p>a fórmula de Δ é: ${nb}<sup>2</sup> -4.${na}.${nc}</p>`
+        res.innerHTML = `<p>A equação atual é ${na}x<sup>2</sup> + ${formatarNumero(nb)}x + ${formatarNumero(nc)} = ${ig}</p>`
+        res.innerHTML += `<p>a fórmula de Δ é: ${formatarNumero(nb)}<sup>2</sup> - 4 • ${formatarNumero(na)} • ${formatarNumero(nc)}</p>`
         res.innerHTML += `<p>o valor de Δ é: ${delta}</p>`
 
         if (delta < 0) {
             res.innerHTML += `Não tem como prosseguir pois o Δ esta negativo`
         } else {
-            var raizdelta = Math.sqrt(delta)
-            var x1 = (-nb + raizdelta) / (2 * na)
-            var x2 = (-nb - raizdelta) / (2 * na)
+            var rdelta = Math.sqrt(delta)
+            var x1 = (-nb - rdelta) / (2 * na)
+            var x2 = (-nb + rdelta) / (2 * na)
             
             
             res.innerHTML += `<p>A formula será: 
             <math>
                 <mfrac>
                     <mrow>
-                        <mo>-</mo><mi>${nb}</mi> 
+                        <mo>-</mo><mi>${formatarNumero(nb)}</mi> 
                         <mo>±</mo> 
                         <msqrt><mn>${delta}</mn></msqrt>
                     </mrow>
                     <mrow>
-                        <mi>2*${na}</mi>
+                        <mi>2•${formatarNumero(na)}</mi>
                     </mrow>
                 </mfrac>
             </math></p>`
 
-            res.innerHTML += `<p>x<sup>1</sup> = 
+            res.innerHTML += `<p>x' = 
             <math>
                 <mfrac>
                     <mrow>
-                        <mo>-</mo><mi>${nb}</mi> <mo>-</mo><msqrt><mn>${delta}</mn></msqrt>
+                        <mo>-</mo><mi>${formatarNumero(nb)}</mi> <mo>-</mo><mn>${Number.isInteger(rdelta) ? rdelta : rdelta.toFixed(2)}</mn>
                     </mrow>
                     <mrow>
-                        <mi>2*${na}</mi>
+                        <mi>2•${formatarNumero(na)}</mi>
                     </mrow>
                 </mfrac>
             </math></p>`
-            res.innerHTML += `<p>x<sup>2</sup> = 
+            res.innerHTML += `<p>x" = 
             <math>
                 <mfrac>
                     <mrow>
-                        <mo>-</mo><mi>${nb}</mi> <mo>+</mo><msqrt><mn>${delta}</mn></msqrt>
+                        <mo>-</mo><mi>${formatarNumero(nb)}</mi> <mo>+</mo><mn>${Number.isInteger(rdelta) ? rdelta : rdelta.toFixed(2)}</mn>
                     </mrow>
                     <mrow>
-                        <mi>2*${na}</mi>
+                        <mi>2•${formatarNumero(na)}</mi>
                     </mrow>
                 </mfrac>
             </math></p>`
-            res.innerHTML += `<p>x<sup>1</sup> = ${x1.toFixed(2)}</p>`
-            res.innerHTML += `<p>x<sup>2</sup> = ${x2.toFixed(2)}</p>`
+            res.innerHTML += `<p>x' = ${Number.isInteger(x1) ? x1 : x1.toFixed(2)}</p>`
+            res.innerHTML += `<p>x" = ${Number.isInteger(x2) ? x2 : x2.toFixed(2)}</p>`
         }
     }
+}
+
+function formatarNumero(n) {
+    return n < 0 ? `(${n})` : n
 }
