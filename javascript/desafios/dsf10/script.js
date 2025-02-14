f29d4937a6049c421b1aaef3296f74762efdf7f5
João Teles <joaohts2210@gmail.com>
2025-02-14 19:03:47 +0000

    arrumar o desafio 10 para aparecer o simbolo de positivo

--- javascript/desafios/dsf10/script.js
+++ javascript/desafios/dsf10/script.js
@@ -1,12 +1,12 @@
-function formatarNumero(n) {
+function formatarNumeron(n) {
     return n < 0 ? `(${n})` : n
 }
+
 
 function calcular() {
     var numa = document.querySelector('input#numa')
     var numb = document.querySelector('input#numb')
     var numc = document.querySelector('input#numc')
-    var igual = document.querySelector('input#igual')
     var res = document.querySelector('div#res')
 
     if (numa.value.length == 0 || numa.value == 0 || numb.value.length == 0 || numb.value == 0 || numc.value.length == 0 || numc.value == 0) {
@@ -15,19 +15,18 @@ function calcular() {
         var na = Number(numa.value)
         var nb = Number(numb.value)
         var nc = Number(numc.value)
-        var ig = Number(igual.value)
         var delta = nb **2 - 4 * na * nc
 
-        res.innerHTML = `<p>A equação atual é ${na}x<sup>2</sup> + ${formatarNumero(nb)}x + ${formatarNumero(nc)} = ${ig}</p>`
-        res.innerHTML += `<p>a fórmula de Δ é: ${formatarNumero(nb)}<sup>2</sup> - 4 • ${formatarNumero(na)} • ${formatarNumero(nc)}</p>`
+        res.innerHTML = `<p>A equação atual é ${na}x<sup>2</sup> ${formatarNumeron(nb)}x ${formatarNumeron(nc)} = 0</p>`
+        res.innerHTML += `<p>a fórmula de Δ é: ${formatarNumeron(nb)}<sup>2</sup> - 4 • ${formatarNumeron(na)} • ${formatarNumeron(nc)} = 0</p>`
         res.innerHTML += `<p>o valor de Δ é: ${delta}</p>`
 
         if (delta < 0) {
             res.innerHTML += `Não tem como prosseguir pois o Δ esta negativo`
         } else {
             var rdelta = Math.sqrt(delta)
-            var x1 = (-nb - rdelta) / (2 * na)
-            var x2 = (-nb + rdelta) / (2 * na)
+            var x1 = (-nb + rdelta) / (2 * na)
+            var x2 = (-nb - rdelta) / (2 * na)
             
             
             res.innerHTML += `<p>A formula será: 
@@ -48,7 +47,7 @@ function calcular() {
             <math>
                 <mfrac>
                     <mrow>
-                        <mo>-</mo><mi>${formatarNumero(nb)}</mi> <mo>-</mo><mn>${Number.isInteger(rdelta) ? rdelta : rdelta.toFixed(2)}</mn>
+                        <mo>-</mo><mi>${formatarNumero(nb)}</mi> <mo>+</mo><mn>${Number.isInteger(rdelta) ? rdelta : rdelta.toFixed(2)}</mn>
                     </mrow>
                     <mrow>
                         <mi>2•${formatarNumero(na)}</mi>
@@ -59,7 +58,7 @@ function calcular() {
             <math>
                 <mfrac>
                     <mrow>
-                        <mo>-</mo><mi>${formatarNumero(nb)}</mi> <mo>+</mo><mn>${Number.isInteger(rdelta) ? rdelta : rdelta.toFixed(2)}</mn>
+                        <mo>-</mo><mi>${formatarNumero(nb)}</mi> <mo>-</mo><mn>${Number.isInteger(rdelta) ? rdelta : rdelta.toFixed(2)}</mn>
                     </mrow>
                     <mrow>
                         <mi>2•${formatarNumero(na)}</mi>
