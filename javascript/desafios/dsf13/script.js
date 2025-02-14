4e3ea036c91461c23fd20562c86c3ddf41aaf853
João Teles <joaohts2210@gmail.com>
2025-02-14 16:48:46 +0000

    desafios completos, aula com o guanabara finalizadogit add .!

--- javascript/desafios/dsf13/script.js
+++ javascript/desafios/dsf13/script.js
@@ -1,19 +1,22 @@
 function calcular() {
-    var nm = document.querySelector('input#nm')
+    var nome = document.querySelector('input#nm')
     var t1 = document.querySelector('input#t1')
     var t2 = document.querySelector('input#t2')
     var t3 = document.querySelector('input#t3')
     var res = document.querySelector('div#res')
+    var nm = nome.value
     var n1 = Number(t1.value)
     var n2 = Number(t2.value)
     var n3 = Number(t3.value)
     var md = (n1 + n2 + n3) / 3
 
-    if (t1.length == 0 || t2.length == 0 || t3.length == 0 || t1 > 10 || t2 > 10 || t3 > 10) {
-        alert('[ERRO] Digite suas notas corretamente')
-    } else if (md >= 6 && md == 10) {
-        res.innerHTML = `<p>Parabéns ${nm}, você está APROVADO!!!</p>`
+    if (nm.length == 0 || t1.length == 0 || t2.length == 0 || t3.length == 0 || t1 > 10 || t2 > 10 || t3 > 10) {
+        alert('[ERRO] Preencha todas as lacunas corretamente')
+    } else if (md >= 6) {
+        res.innerHTML = `<p>Sua média final é ${md.toFixed(1)}</p>`
+        res.innerHTML += `<p>Parabéns ${nm}, você está <span class="aprovado">APROVADO!!!</span></p>`
     } else {
-        res.innerHTML = `<p>${nm}, infelimente você está REPROVADO!!!</p>`
+        res.innerHTML = `<p>Sua média final é ${md.toFixed(1)}</p>`
+        res.innerHTML += `<p>${nm}, infelimente você está <span class="reprovado">REPROVADO!!!</span></p>`
     }
 }
